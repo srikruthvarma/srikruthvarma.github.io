bffefa21bb7c023d6a3310ac1374b6f26bd3c439
Srikruth Varma <youremail@example.com>
2025-06-12 12:28:57 +0530

    update 14

--- A5/script.js
+++ A5/script.js
@@ -1,51 +1,67 @@
 const users = [];
-let user={}
+let user = {};
+
+const showMenu = () => {
+  return `
+    <div class="menu-bar">
+      <a href="#" onclick="showHome()">Home</a>
+      <a href="#" onclick="showContact()">Contact</a>
+      <a href="#" onclick="showLogin()">Logout</a>
+    </div>
+  `;
+};
+
 const showLogin = () => {
   let str = `
-    <div>
-    <h1>Login Form</h1>
-    <p><div id="dvMsg"></div></p>
-    <p><input type="text" id="txtEmail"></p>
-    <p><input type="password" id="txtPass"></p>
-    <p><button onclick='validateUser()'>Log In</button></p>
-    <p><button onclick='showRegister()'>Create Account</button></p>
+    <div class="App-Container">
+      <h1>Login Form</h1>
+      <p><div id="dvMsg" style="color: red;"></div></p>
+      <p><input type="text" id="txtEmail" placeholder="Email"></p>
+      <p><input type="password" id="txtPass" placeholder="Password"></p>
+      <p><button onclick='validateUser()'>Log In</button></p>
+      <p><button onclick='showRegister()'>Create Account</button></p>
     </div>
-    `;
+  `;
   root.innerHTML = str;
 };
 
 const showRegister = () => {
   let str = `
-    <h1>Register Form</h1>
-    <p><input type="text" id="txtName"></p>
-     <p><input type="text" id="txtEmail"></p>
-    <p><input type="password" id="txtPass"></p>
-    <button onclick='addUser()'>Register</button>
-    <hr>
-    <button onClick='showLogin()'>Alread a Member? Login here...</button>
-    `;
+    <div class="App-Container">
+      <h1>Register Form</h1>
+      <p><input type="text" id="txtName" placeholder="Name"></p>
+      <p><input type="text" id="txtEmail" placeholder="Email"></p>
+      <p><input type="password" id="txtPass" placeholder="Password"></p>
+      <button onclick='addUser()'>Register</button>
+      <hr>
+      <button onclick='showLogin()'>Already a Member? Login here...</button>
+    </div>
+  `;
   root.innerHTML = str;
 };
 
 const showHome = () => {
   let str = `
-    <h1>Welcome ${user.name}</h1>
-    <hr>
-    <p><select id="actionSelect">
-     <option value=0>--select--</option>
-      <option value=1>Deposit</option>
-      <option value=2>Withdraw</option>
-      </select></p>
-      <p>
-      <input type='number' id='txtAmount'>
-      </p>
-      <p><button onclick="handleTransaction()">Submit</button>
-      </p>
+    ${showMenu()}
+    <div class="App-Container">
+      <h1>Welcome, ${user.name}!</h1>
+      <p>This is your Home Page.</p>
+      <button onclick="showLogin()">Logout</button>
+    </div>
+  `;
+  root.innerHTML = str;
+};
 
-    <button onclick='showLogin()'>Logout</button>
-    <hr>
-    <p>Current balance:${user.balance}</p>
-    `;
+const showContact = () => {
+  let str = `
+    ${showMenu()}
+    <div class="App-Container">
+      <h1>Contact Us</h1>
+      <p><strong>Email:</strong> srikruthvarma@gmail.com.</p>
+      <p><strong>Phone:</strong> 9100053005</p>
+      <p><strong>Address:</strong> opp to tech mahindra Hyderabad, India</p>
+    </div>
+  `;
   root.innerHTML = str;
 };
 
@@ -53,8 +69,7 @@ const addUser = () => {
   const obj = {
     name: document.getElementById("txtName").value,
     email: document.getElementById("txtEmail").value,
-    pass: document.getElementById("txtPass").value,
-    balance:0
+    pass: document.getElementById("txtPass").value
   };
   users.push(obj);
   showLogin();
@@ -63,42 +78,12 @@ const addUser = () => {
 const validateUser = () => {
   let email = document.getElementById("txtEmail").value;
   let pass = document.getElementById("txtPass").value;
-   user = users.find(
-    (e) => e.email === email && e.pass === pass
-  )
+
+  user = users.find((e) => e.email === email && e.pass === pass);
+
   if (user) {
     showHome();
   } else {
-    dvMsg.innerHTML = "Access Denied";
+    document.getElementById("dvMsg").innerText = "Access Denied";
   }
 };
-
-const handleTransaction = () => {
-  const action = document.getElementById("actionSelect").value;
-  const amount = Number(document.getElementById("txtAmount").value);
-
-  if (action === "1") {
-    user.balance += amount;
-
-  } else if (action === "2") {
-    if (amount > user.balance) {
-      alert("insufficient balance");
-      return;
-    }
-    user.balance -= amount;
-
-  //  }else if (action === "3") {
-  //     if (amount > user.balance) {
-  //       alert("insuffecient balance!");
-  //       return;
-  //     }
-
-    }
-  showHome();
-};
-
-// const toggleTransfer = () => {
-//   const action = document.getElementById("actionSelect").value;
-//   const transferFund = document.getElementById("transferFund");
-//   if (action ==="3") {}
-// }
